1c935a5d0449b5a593480b0c1d4014bbd2dabaf3
thiagola92 <thiagola92@gmail.com>
2022-05-17 08:21:55 -0300

    Add mercado livre

---
+++ page_fetcher/marketplaces/mercado_livre.py
@@ -0,0 +1,31 @@
+import asyncio
+from collections.abc import AsyncGenerator
+
+from aiohttp import ClientSession
+from la_headers import generate_random_headers
+
+from page_fetcher.abstractions import Marketplace
+
+
+class Rihappy(Marketplace):
+    async def fetch(self, urls: list[str]) -> AsyncGenerator[str, str | None]:
+        headers = generate_random_headers(os=["linux"], browser=["chrome"])
+
+        async with ClientSession(headers=headers) as session:
+            for url in urls:
+                while url:
+                    async with session.get(url) as response:
+                        self._logger.info(
+                            event="Response received",
+                            status=response.status,
+                            url=url,
+                        )
+
+                        await self._raise_for_status(response.status)
+                        response.raise_for_status()
+
+                        url = yield await response.text()
+
+    async def _cooldown(self) -> None:
+        self._logger.debug(event=f"Start cooldown")
+        await asyncio.sleep(1)
